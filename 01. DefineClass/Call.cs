08bf9df755df2e21890336a0ad73e1537cab0c59
juvemar <martoo.atanasov@gmail.com>
2015-03-15 20:23:54 +0200

    last

---
+++ 01. DefineClass/Call.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Mobile
+{
+    public class Call
+    {
+        public DateTime dateAndTime;
+        public string number;
+        public ulong duration;
+
+        public Call(DateTime dateTime, string number, ulong duration)
+        {
+            this.dateAndTime = dateTime;
+            this.Number = number;
+            this.Duration = duration;
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return this.dateAndTime;
+            }
+            set
+            {
+                this.dateAndTime = value;
+            }
+        }
+
+        public string Number
+        {
+            get
+            {
+                return this.number;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Number cannot be null or empty!");
+                }
+                else
+                {
+                    this.number = value;
+                }
+            }
+        }
+        
+        public ulong Duration
+        {
+            get
+            {
+                return this.duration;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("Duration of a call cannot be less than 0!");
+                }
+                else
+                {
+                    this.duration = value;
+                }
+            }
+        }
+    }
+}
---
+++ 01. DefineClass/GSMCallHistoryTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile
+{
+    public class GSMCallHistoryTest
+    {
+        private const decimal pricePerMin = 0.37m;
+
+        static GSM phone = new GSM("K800", "Sony Ericsson", 300, "Ganio", new Display(3, 5),
+                                new Battery("MI250", 150, 300, BatteryType.LiIon));
+        static Call firstCall = new Call(new DateTime(2014, 3, 1, 3, 30, 45), "0888343434", 123123);
+        static Call secondCall = new Call(new DateTime(2014, 4, 2, 2, 15, 34), "0888243644", 110243);
+        static Call thirdCall = new Call(new DateTime(2014, 3, 8, 9, 23, 2), "0888353476", 115032);
+
+        static void Main()
+        {
+            
+
+        }
+        public void AddingCalls()
+        {
+            phone.AddCalls(firstCall);
+            phone.AddCalls(secondCall);
+            phone.AddCalls(thirdCall);
+        }
+
+        public void PrintCallHistory(GSM phone)
+        {
+            foreach (var item in phone.CallHistory)
+            {
+                DateTime currentDate = item.dateAndTime;
+                string currentNumber = item.number;
+                ulong currentDuration = item.duration;
+
+                Console.WriteLine("Date: {0}\nPhone number: {1}\nCall duration: {2} sec", currentDate, currentNumber, currentDuration);
+                Console.WriteLine();
+            }
+        }
+
+        public void CalculateTotalPrice(GSM phone)
+        {
+            Console.WriteLine("The total price of the calls is {0} cents.", phone.TotalCallsPrice(pricePerMin));
+        }
+
+        public void CalculateTotalPriceAfterRemove(GSM phone)
+        {
+            Console.WriteLine("The total price of the calls after the erasure is {0} cents.", phone.TotalCallsPrice(pricePerMin));
+        }
+
+        public Call DeleteLongestCall(GSM phone)
+        {
+            decimal max = phone.CallHistory[0].duration;
+            int toBeDeleted = 0;
+
+            for (int i = 1; i < phone.CallHistory.Count; i++)
+            {
+                if (phone.CallHistory[i].duration > max)
+                {
+                    toBeDeleted = i;
+                }
+            }
+
+            return phone.CallHistory[toBeDeleted];
+        }
+
+        public void ClearHistory(GSM phone)
+        {
+            phone.CallHistory.Clear();
+        }
+    }
+}
